ef81eac49038acd2ec43f381c5cef1e111c7cccc
Fox-Drone <sacha.fox26@gmail.com>
2026-06-09 12:46:59 +0200

    Update metric.rs

--- backend/src/models/metric.rs
+++ backend/src/models/metric.rs
@@ -1,5 +1,6 @@
 use chrono::{DateTime, Utc};
 use serde::{Deserialize, Serialize};
+use serde_json::Value;
 use uuid::Uuid;
 
 #[derive(Debug, Clone, Serialize, Deserialize)]
@@ -8,6 +9,7 @@ pub struct Metric {
     pub host: String,
     pub metric_type: String,
     pub value: f64,
-    pub timestamp: DateTime<Utc>,
     pub source: String,
+    pub labels: Option<Value>,
+    pub timestamp: DateTime<Utc>,
 }
