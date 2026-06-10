e3dc4bd2969c42b407530f421a38c413b2dd02bf
Fox-Drone <sacha.fox26@gmail.com>
2026-06-10 14:35:34 +0200

    Update api.ts

--- frontend/src/services/api.ts
+++ frontend/src/services/api.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export const api = axios.create({
-  baseURL: "http://192.168.118.129:3000",
+  baseURL: "/api",
   headers: {
     "Content-Type": "application/json",
   },
